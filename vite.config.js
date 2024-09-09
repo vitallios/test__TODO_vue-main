e1f3592a8a052b0a692c8a2e6642342f45b020e4
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-09-09 17:15:15 +0300

    gs-pages-4

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/test__TODO_vue-main/',
+  publicPath: '/test__TODO_vue-main',
   plugins: [
     vue(),
   ],
