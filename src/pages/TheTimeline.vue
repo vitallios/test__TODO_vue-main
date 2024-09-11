d27a1262bd4eec76479dd0459173b99bdab37ebd
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-09-11 17:48:37 +0300

    update function

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,5 +1,10 @@
 <template>
- <section class="container">
-  T I M E L I N E
+ <section class="container mt-7">
+  <ul>
+   <li v-for="i in [0, 1, 2, 3, 4, 5, 6]" :key="i"
+   class=" relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
+    <a href="#" class=" rounded bg-gray-100 px-2 font-mono text-lg text-gray-500 absolute -top-4 left-1/2 -translate-x-1/2" >0{{ i }}:00</a>
+   </li>
+  </ul>
  </section>
 </template>
