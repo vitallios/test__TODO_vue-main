238e696b87ed64418a7fe0ad02ce8be36a690756
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-09-12 11:27:28 +0300

    add hour

---
+++ src/components/TimlineItem.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['timlineItem'])
+</script>
+
+<template>
+<li
+   class=" relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
+    <a href="#" class=" rounded bg-gray-100 px-2 font-mono text-lg text-gray-500 absolute -top-4 left-1/2 -translate-x-1/2" >0{{ timlineItem.hour }}:00</a>
+   </li>
+</template>
--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { generateTimlineItems } from '../function';
+import TimlineItem from '@/components/TimlineItem.vue';
 const timlineItems = generateTimlineItems();
 
 
@@ -8,10 +9,8 @@ const timlineItems = generateTimlineItems();
 <template>
  <section class="container mt-7">
   <ul>
-   <li v-for="{ hour } in timlineItems" :key="hour"
-   class=" relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
-    <a href="#" class=" rounded bg-gray-100 px-2 font-mono text-lg text-gray-500 absolute -top-4 left-1/2 -translate-x-1/2" >0{{ hour }}:00</a>
-   </li>
-  </ul>
+   <TimlineItem  v-for="timlineItem in timlineItems" :key="timlineItem.hour" :timline-item="timlineItem " />
+   </ul>
+  
  </section>
 </template>
