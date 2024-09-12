7b905c794d5b8d0a714b27fcb7a381d55985ed0e
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-09-12 11:34:19 +0300

    add hour

--- src/components/TimlineItem.vue
+++ src/components/TimlineItem.vue
@@ -1,10 +1,15 @@
 <script setup>
-defineProps(['timlineItem'])
+const props = defineProps(['timlineItem'])
+const hourLinkClasses = [
+   'rounded px-2 font-mono text-lg  absolute -top-4 left-1/2 -translate-x-1/2',
+   props.timlineItem.hour === new Date().getHours()
+      ? 'bg-purple-900 font-black text-white'
+      : 'bg-gray-100 text-gray-500'
+]
 </script>
 
 <template>
-<li
-   class=" relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
-    <a href="#" class=" rounded bg-gray-100 px-2 font-mono text-lg text-gray-500 absolute -top-4 left-1/2 -translate-x-1/2" >0{{ timlineItem.hour }}:00</a>
+   <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
+      <a href="#" :class="hourLinkClasses">{{ timlineItem.hour }}:00</a>
    </li>
 </template>
